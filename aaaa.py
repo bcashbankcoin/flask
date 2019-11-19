e8f1ed8af365a6810d234498c157caaf000a09fd
Bankcoin BCASH <46301303+bcashbankcoin@users.noreply.github.com>
2019-11-20 00:18:25 +0600

    Delete aaaa.py

--- aaaa.py
+++
@@ -1,6 +0,0 @@
-"""
-image = request.files['image']
-        if image and image.filename:
-            filename = image.filename
-            image.save(os.path.join('static/images', filename)) 
-""" 
